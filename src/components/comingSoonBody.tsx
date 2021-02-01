32eea713e34e3b0805fdd9a1696bbda29d5df9fe
Anurag5086 <anuraggupta5086@gmail.com>
2021-02-01 14:03:03 +0400

    added

--- src/components/comingSoonBody.tsx
+++ src/components/comingSoonBody.tsx
@@ -10,7 +10,7 @@ export default function ComingSoonBody(){
         e.preventDefault();
         let emailUser = email;
         console.log(emailUser);
-        const res = await fetch('http://localhost:3000/api/emailSubscribe',{
+        const res = await fetch('https://progarten-assign.vercel.app/api/emailSubscribe',{
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
