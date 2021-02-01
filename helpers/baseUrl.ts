c48c03c51765c7c4e56b8c648e018e53abc413ff
Anurag5086 <anuraggupta5086@gmail.com>
2021-02-01 13:18:40 +0400

    deployed

--- helpers/baseUrl.ts
+++ helpers/baseUrl.ts
@@ -1,2 +1,2 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? "https://mywebsite.com": "http://localhost:3000"
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://progarten-assign.herokuapp.com": "http://localhost:3000"
 export default baseUrl
